604d6dd9bc1275cb46aa9a5aa8b6112f06990240
saadiahumayun <34272512+saadiahumayun@users.noreply.github.com>
2023-05-19 01:45:45 +0500

    Update app.py

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -125,7 +125,7 @@ features_df = pd.DataFrame([features])
 st.subheader ('Please adjust feature values from the sidebar.')
 st.dataframe(features_df)
 # Load the LIME explainer model
-explainer = LimeTabularExplainer(training_data.values, feature_names=['clonesize','honeybee', 'bumbles', 'andrena', 'osmia', 'MaxOfUpperTRange', 'MinOfUpperTRange',
+explainer = LimeTabularExplainer(training_data.values, feature_names=['id','clonesize','honeybee', 'bumbles', 'andrena', 'osmia', 'MaxOfUpperTRange', 'MinOfUpperTRange',
                                                                      'AverageOfUpperTRange','MaxOfLowerTRange', 'MinOfLowerTRange','AverageOfLowerTRange',
                                                                      'RainingDays','AverageRainingDays','fruitset','fruitmass','seeds'], mode='regression')
 
