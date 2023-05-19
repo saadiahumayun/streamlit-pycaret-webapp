9500bbd11b1c47d3080fbf926e908a322c7ec24f
saadiahumayun <34272512+saadiahumayun@users.noreply.github.com>
2023-05-19 08:39:10 +0500

    Update app.py

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -135,7 +135,7 @@ if st.button('Predict'):
 explainer = LimeTabularExplainer(training_data.values, feature_names=training_data.columns.tolist(), mode='regression')
 
 def predict_fn(X):
-    return model.predict(features_df)
+    return model.predict()
 
 if st.button('Explain'):
     
