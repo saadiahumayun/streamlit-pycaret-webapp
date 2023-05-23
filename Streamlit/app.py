7ace9cf29c6db6f733b73a06c67c98c01d5fb4fa
saadiahumayun <34272512+saadiahumayun@users.noreply.github.com>
2023-05-23 20:14:24 +0500

    Update app.py

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -160,23 +160,9 @@ arr = features_df.to_numpy()
     #return predictions
 
 
-if st.button('Explain with SHAP'):
+#if st.button('Explain with SHAP'):
 
-    def st_shap(plot, height=None):
-        shap_html = f"<head>{shap.getjs()}</head><body>{plot.html()}</body>"
-        components.html(shap_html, height=height)
-
-    st.title("SHAP in Streamlit")
-    # explain the model's predictions using SHAP
-    # (same syntax works for LightGBM, CatBoost, scikit-learn and spark models)
-    explainer = shap.TreeExplainer(gb_regressor)
-    shap_values = explainer.shap_values(X_train)
-
-    # visualize the first prediction's explanation (use matplotlib=True to avoid Javascript)
-    st_shap(shap.force_plot(explainer.expected_value, shap_values[0,:], features_df.iloc[0,:]))
-
-    # visualize the training set predictions
-    st_shap(shap.force_plot(explainer.expected_value, shap_values, X_train), 400)
+    
     
 
 if st.button('Explain with LIME'):
