a48432377196ab7dd7d985edece9aa96f02d2b99
saadiahumayun <34272512+saadiahumayun@users.noreply.github.com>
2023-05-24 09:55:54 +0500

    Update app.py

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -271,7 +271,8 @@ if st.button('Generate Counterfactuals'):
                                   total_CFs=5, 
                                   desired_range=[5600.0, 8900.0])
     # Visualize it
-    cf.visualize_as_dataframe(show_only_changes=True)
+    ins1=pd.DataFrame([cf.visualize_as_list()])
+    ins1
     #components.html(cf.as_html(), height=800)
 
     # %% Create feasible (conditional) Counterfactuals
